83b718a6b6b8c7121eff43ab442293f17b7c0079
mark <markpackham1@gmail.com>
2022-05-18 15:34:12 +0100

    style single listing
    https://youtu.be/MYyJ4PuL4pY?t=4986

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,2 +1,8 @@
-<h2>{{ $listing['title'] }}</h2>
-<p>{{ $listing['description'] }}</p>
+@extends('./components/layout')
+
+@section('content')
+    @include('partials._search')
+
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+@endsection
