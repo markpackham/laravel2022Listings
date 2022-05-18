271a6769a49d867bbf479757d5ca672f5d267878
mark <markpackham1@gmail.com>
2022-05-18 15:15:47 +0100

    fix styling
    https://youtu.be/MYyJ4PuL4pY?t=4673

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,7 +7,7 @@
             @foreach ($listings as $listing)
                 <div class="bg-gray-50 border border-gray-200 rounded p-6">
                     <div class="flex">
-                        <img class="hidden w-48 mr-6 md:block" src="images/acme.png" alt="" />
+                        <img class="hidden w-48 mr-6 md:block" src="images/no-image.png" alt="" />
                         <div>
                             <h3 class="text-2xl">
                                 <a href="show.html">{{ $listing->title }}</a>
@@ -32,8 +32,7 @@
                                 </li>
                             </ul>
                             <div class="text-lg mt-4">
-                                <i class="fa-solid fa-location-dot"></i> Boston,
-                                MA
+                                <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
                             </div>
                         </div>
                     </div>
