33f32f3fb4d8aa7ee339bbd7be17688e378dcb9a
mark <markpackham1@gmail.com>
2022-05-18 14:36:46 +0100

    experiment extending layout
    https://youtu.be/MYyJ4PuL4pY?t=4286

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,11 +1,16 @@
-<h1>{{ $heading }}</h1>
+@extends('test')
 
-@unless(count($listings) == 0)
-    @foreach ($listings as $listing)
-        <h2>{{ $listing['title'] }}</h2>
-        <a href="/listings/{{ $listing['id'] }}">Listing Id {{ $listing['id'] }}</a>
-        <p>{{ $listing['description'] }}</p>
-    @endforeach
-@else
-    <p>No listings</p>
-@endunless
+@section('content')
+    <h1>{{ $heading }}</h1>
+
+    @unless(count($listings) == 0)
+        @foreach ($listings as $listing)
+            <h2>{{ $listing['title'] }}</h2>
+            <a href="/listings/{{ $listing['id'] }}">Listing Id {{ $listing['id'] }}</a>
+            <p>{{ $listing['description'] }}</p>
+        @endforeach
+    @else
+        <p>No listings</p>
+    @endunless
+
+@endsection
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -27,38 +27,38 @@
 
 <body class="mb-48">
     <nav class="flex justify-between items-center mb-4">
-        <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
+        <a href="/"><img class="w-24" src="" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
-            @auth
-                <li>
-                    <span class="font-bold uppercase">
-                        Welcome
-                    </span>
-                </li>
-                <li>
-                    <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
-                </li>
-                <li>
-                    <form class="inline" method="POST" action="/logout">
-                        @csrf
-                        <button type="submit">
-                            <i class="fa-solid fa-door-closed"></i> Logout
-                        </button>
-                    </form>
-                </li>
-            @else
-                <li>
-                    <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
-                </li>
-                <li>
-                    <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
-                </li>
-            @endauth
+
+            <li>
+                <span class="font-bold uppercase">
+                    Welcome
+                </span>
+            </li>
+            <li>
+                <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
+            </li>
+            <li>
+                <form class="inline" method="POST" action="/logout">
+                    @csrf
+                    <button type="submit">
+                        <i class="fa-solid fa-door-closed"></i> Logout
+                    </button>
+                </form>
+            </li>
+
+            <li>
+                <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+            </li>
+            <li>
+                <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+            </li>
+
         </ul>
     </nav>
 
     <main>
-
+        @yield('content')
     </main>
     <footer
         class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
@@ -67,7 +67,7 @@
         <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
     </footer>
 
-    <x-flash-message />
+    {{-- <x-flash-message /> --}}
 </body>
 
 </html>
