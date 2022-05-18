e2637f1c401d4bb13b9c0393a50264e6cd4380bc
mark <markpackham1@gmail.com>
2022-05-18 15:19:29 +0100

    use asset function to handle images
    https://youtu.be/MYyJ4PuL4pY?t=4738

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,40 +27,40 @@
 
 <body class="mb-48">
     <nav class="flex justify-between items-center mb-4">
-        <a href="/"><img class="w-24" src="" alt="" class="logo" /></a>
+        <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt=""
+                class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
-
-            <li>
-                <span class="font-bold uppercase">
-                    Welcome
-                </span>
-            </li>
-            <li>
-                <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
-            </li>
-            <li>
-                <form class="inline" method="POST" action="/logout">
-                    @csrf
-                    <button type="submit">
-                        <i class="fa-solid fa-door-closed"></i> Logout
-                    </button>
-                </form>
-            </li>
-
-            <li>
-                <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
-            </li>
-            <li>
-                <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
-            </li>
-
+            @auth
+                <li>
+                    <span class="font-bold uppercase">
+                        Welcome {{ auth()->user()->name }}
+                    </span>
+                </li>
+                <li>
+                    <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
+                </li>
+                <li>
+                    <form class="inline" method="POST" action="/logout">
+                        @csrf
+                        <button type="submit">
+                            <i class="fa-solid fa-door-closed"></i> Logout
+                        </button>
+                    </form>
+                </li>
+            @else
+                <li>
+                    <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+                </li>
+                <li>
+                    <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+                </li>
+            @endauth
         </ul>
     </nav>
 
     <main>
         @yield('content')
     </main>
-
     <footer
         class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
         <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,7 +7,7 @@
             @foreach ($listings as $listing)
                 <div class="bg-gray-50 border border-gray-200 rounded p-6">
                     <div class="flex">
-                        <img class="hidden w-48 mr-6 md:block" src="images/no-image.png" alt="" />
+                        <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
                         <div>
                             <h3 class="text-2xl">
                                 <a href="show.html">{{ $listing->title }}</a>
