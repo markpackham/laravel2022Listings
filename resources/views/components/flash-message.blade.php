88be9cf0b7ce79c7ba6f5d00ec18948a4cc272fd
mark <markpackham1@gmail.com>
2022-05-19 18:56:21 +0100

    comment where apline.js used
    https://youtu.be/MYyJ4PuL4pY?t=9416

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,4 +1,5 @@
 @if (session()->has('message'))
+    {{-- We use apline.js in the div to hide the message after 3 seconds --}}
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
         class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
         <p>
