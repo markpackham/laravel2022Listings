4e086da7f2330d8fc9c297daf236ee1557ff1a56
mark <markpackham1@gmail.com>
2022-05-20 11:14:33 +0100

    provide description of logo in alt
    https://youtu.be/MYyJ4PuL4pY?t=11042

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -8,7 +8,7 @@
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6"
                     src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png') }}"
-                    alt="" />
+                    alt="{{ $listing->company }} logo" />
 
                 <h3 class="text-2xl mb-2">
                     {{ $listing->title }}
