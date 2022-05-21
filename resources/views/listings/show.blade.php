c9bf15f324ade7e4fa3f3eb4425ea9229573259e
mark <markpackham1@gmail.com>
2022-05-21 16:10:03 +0100

    hide from show edit and delete from unauthorized user

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -38,17 +38,19 @@
                     </div>
                 </div>
 
-                <x-card class="mt-4 p-2 flex space-x-6">
-                    <a href="/listings/{{ $listing->id }}/edit">
-                        <i class="fa-solid fa-pencil"></i> Edit
-                    </a>
+                @if ($listing->user_id == auth()->id())
+                    <x-card class="mt-4 p-2 flex space-x-6">
+                        <a href="/listings/{{ $listing->id }}/edit">
+                            <i class="fa-solid fa-pencil"></i> Edit
+                        </a>
 
-                    <form method="POST" action="/listings/{{ $listing->id }}">
-                        @csrf
-                        @method('DELETE')
-                        <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
-                    </form>
-                </x-card>
+                        <form method="POST" action="/listings/{{ $listing->id }}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
+                        </form>
+                    </x-card>
+                @endif
 
             </div>
         </x-card>
